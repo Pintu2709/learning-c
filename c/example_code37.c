7be473515d6e20d3e856c9b98d4e80d35aeec495
Kumar <kumarpintu796@gmail.com>
2022-03-09 14:44:32 +0530

    two dimenstional array example

---
+++ c/example_code37.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int grades[2][2] = {1, 2, 3, 4};
+    int i, j;
+    for (i = 0; i, 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            print("grades[%d]=%d\n", i, j, grades[i][j]);
+        }
+    }
+    return 0;
+}
