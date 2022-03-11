5472c589a39464a3cfda1cf97ddd9e18185414d7
Kumar <kumarpintu796@gmail.com>
2022-03-11 10:38:57 +0530

    switch case example

--- c/w3school.c/example33.c
+++ c/w3school.c/example33.c
@@ -2,6 +2,6 @@
 int main()
 {
     int time = 20;
-    (time < 18) ? printf("Good day.") : printf("Good evening.");
+    (time < 18) ? printf("Good day.") : printf("Good evening");
     return 0;
 }
